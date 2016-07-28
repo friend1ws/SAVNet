fb7041156843949c04d74a67dac5d7cae2d34596
friend1ws <friend1ws@gmail.com>
2016-07-28 17:47:52 +0900

    add software base files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='genomon_splicing_mutation',
+      version='0.1.0',
+      description='Python tools for detecting mutations causing splicing changes',
+      author='Yuichi Shiraishi',
+      author_email='friend1ws@gamil.com',
+      url='https://github.com/Genomon-Project/GenomonSplicingMutation.git',
+      package_dir = {'': 'lib'},
+      packages=['genomon_splicing_mutation'],
+      scripts=['genomon_splicing_mutation'],
+      license='GPL-3'
+     )
+
