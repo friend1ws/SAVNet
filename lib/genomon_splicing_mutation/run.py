855089ad5f5d36d57e84db9f408da8c324b377e8
friend1ws <friend1ws@gmail.com>
2016-07-28 17:59:51 +0900

    add merge_SJ function

--- lib/genomon_splicing_mutation/run.py
+++ lib/genomon_splicing_mutation/run.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
-def main(args):
-    pass
+import utils
 
+def main(args):
+
+    utils.merge_SJ(args.sample_list_file, args.output_prefix + ".SJ_merged.txt", args.pooled_control_file, 2)
+
+ 
