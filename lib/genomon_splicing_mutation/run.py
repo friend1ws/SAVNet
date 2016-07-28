78848594140f4b7f586f82d4bf187ed0692747d7
friend1ws <friend1ws@gmail.com>
2016-07-28 22:33:54 +0900

    add merge_mut function

--- lib/genomon_splicing_mutation/run.py
+++ lib/genomon_splicing_mutation/run.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python
 
+import os
 import utils
 
 def main(args):
 
-    utils.merge_SJ(args.sample_list_file, args.output_prefix + ".SJ_merged.txt", args.pooled_control_file, 2)
+    output_prefix_dir = os.path.dirname(args.output_prefix)
+    if output_prefix_dir != "" and not os.path.exists(output_prefix_dir):
+       os.makedirs(output_prefix_dir)
 
- 
+    # utils.merge_SJ(args.sample_list_file, args.output_prefix + ".SJ_merged.txt", args.pooled_control_file, 2)
+
+    utils.merge_mut(args.sample_list_file, args.output_prefix + ".mut_merged.txt")
+
