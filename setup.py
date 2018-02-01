e2eb8b016a61951cd95ddcff0e640b5aa81ddeb3
friend1ws <friend1ws@gmail.com>
2018-02-01 12:05:35 +0900

    improve package structure

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='savnet',
-      version='0.2.1',
-      description='Python tools for detecting mutations causing splicing changes',
-      author='Yuichi Shiraishi',
-      author_email='friend1ws@gamil.com',
-      url='https://github.com/Genomon-Project/GenomonSplicingMutation.git',
-      package_dir = {'': 'lib'},
-      packages=['savnet'],
-      scripts=['savnet'],
-      license='GPL-3'
-     )
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'savnet',
+    version = '0.3.0b1',
+    description='Python tools for detecting mutations causing splicing changes',
+    url = 'https://github.com/friend1ws/SAVNet',
+    author = 'Yuichi Shiraishi',
+    author_email = 'friend1ws@gamil.com',
+    license = 'GPLv3',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+
+    packages = find_packages(exclude = ['tests']),
+    package_data={'sv_utils': ['data/*']},
+
+    install_requires = [],
+    entry_points = {'console_scripts': ['savnet = savnet:main']}
+
+)
+
 
