fecd5fea83047c6e5726e15b4050ec2018721efe
friend1ws <friend1ws@gmail.com>
2018-02-03 01:34:05 +0900

    modify check_download.py

--- tests/check_download.py
+++ tests/check_download.py
@@ -8,7 +8,7 @@ def check_download(url, output_path):
        os.makedirs(os.path.dirname(output_path))
 
     if not os.path.exists(output_path):
-        print >> sys.stderr "Downloading: " + output_path
+        print >> sys.stderr, "Downloading: " + output_path
         ufile = urllib2.urlopen(url)
         with open(output_path, 'w') as hout:
             for x in ufile:
