e226a5d8726c9f238e9158d3953ccab3b872bf5b
friend1ws <friend1ws@gmail.com>
2018-02-22 17:27:29 +0900

    Logging information

--- savnet/utils.py
+++ savnet/utils.py
@@ -3,6 +3,8 @@
 import math, sys, os, logging
 from subprocess import Popen, PIPE
 
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt="%Y-%m-%d %I:%M:%S", level=logging.INFO)
+
 def soft_max(x):
     x_max = max(x)
     return(x_max + math.log(sum([math.exp(y - x_max) for y in x]))) 
@@ -10,7 +12,7 @@ def soft_max(x):
 
 def median(numbers):
     if len(numbers) == 0:
-        print >> sys.stderr, "Vector of zero length was put to median function. Return 0"
+        logging.error("Vector of zero length was put to median function. Return 0")
         return 0
     return (sorted(numbers)[int(round((len(numbers) - 1) / 2.0))] + sorted(numbers)[int(round((len(numbers) - 1) // 2.0))]) / 2.0
 
@@ -31,10 +33,11 @@ def generate_configurations(dim):
 
 
 def is_tool(executable):
+
     proc = Popen(["which", executable], stdout = PIPE, stderr = PIPE)
     out, err = proc.communicate()
     if proc.returncode == 1:
-        print >> sys.stderr, "Executable does not exist: " + executable
+        logging.error("Executable does not exist: " + executable)
         sys.exit(1) 
 
     return True
