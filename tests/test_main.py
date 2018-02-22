990d024a5b2f197ee95d62c5624e3053848ceddc
friend1ws <friend1ws@gmail.com>
2018-02-23 03:06:06 +0900

    Add SV tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -42,13 +42,12 @@ class TestMain(unittest.TestCase):
  
         self.parser = savnet.parser.create_parser()
 
- 
+
     def test1(self):
 
         cur_dir = os.path.dirname(os.path.abspath(__file__))
         tmp_dir = tempfile.mkdtemp()
 
-        print >> sys.stderr, "Creating sample list file for SAVNET."
         make_savnet_input(cur_dir + "/resource/savnet_input.txt", \
                           cur_dir + "/resource/mutation", \
                           cur_dir + "/resource/junction", \
@@ -61,7 +60,6 @@ class TestMain(unittest.TestCase):
         sj_control_file = cur_dir + "/resource/control/SJ_control_2_4.bed.gz"
         ir_control_file = cur_dir + "/resource/control/IR_control_4.bed.gz"
 
-        print >> sys.stderr, "Executing SAVNET."
 
         savnet_args = [sample_list_file, output_prefix, ref_genome, \
                            "--SJ_pooled_control_file", sj_control_file, \
@@ -72,7 +70,7 @@ class TestMain(unittest.TestCase):
         savnet.run.savnet_main(args)
 
         self.assertTrue(317 <= len(open(tmp_dir + "/test.savnet.result.txt", 'r').readlines()) <= 327)
-        # shutil.rmtree(tmp_dir)
+        shutil.rmtree(tmp_dir)
 
 
     def test2(self):
@@ -98,7 +96,6 @@ class TestMain(unittest.TestCase):
         sj_control_file = cur_dir + "/resource/control/SJ_control_2_4.bed.gz"
         ir_control_file = cur_dir + "/resource/control/IR_control_4.bed.gz"
 
-        print >> sys.stderr, "Executing SAVNET."
 
         savnet_args = [sample_list_file, output_prefix, ref_genome, \
                            "--SJ_pooled_control_file", sj_control_file, \
@@ -112,6 +109,34 @@ class TestMain(unittest.TestCase):
         shutil.rmtree(tmp_dir)
 
 
+    def test3(self):
+
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = tempfile.mkdtemp()
+
+        make_savnet_input_sv(cur_dir + "/resource/savnet_input_sv.txt", \
+                             cur_dir + "/resource/sv", \
+                             cur_dir + "/resource/junction", \
+                             cur_dir + "/resource/intron_retention", \
+                             cur_dir + "/resource/chimera", \
+                             cur_dir + "/resource/qc")
+
+        sample_list_file = cur_dir + "/resource/savnet_input_sv.txt"
+        output_prefix = tmp_dir + "/test"
+        ref_genome = cur_dir + "/resource/reference_genome/GRCh37.fa"
+        sj_control_file = cur_dir + "/resource/control/SJ_control_2_4.bed.gz"
+        ir_control_file = cur_dir + "/resource/control/IR_control_4.bed.gz"
+
+        savnet_args = [sample_list_file, output_prefix, ref_genome, "--sv", \
+                           "--SJ_pooled_control_file", sj_control_file, \
+                           "--IR_pooled_control_file", ir_control_file, "--grc"]
+        print "savnet" + ' ' + ' '.join(savnet_args)
+
+        args = self.parser.parse_args(savnet_args)
+        savnet.run.savnet_main(args)
+
+        self.assertTrue(94 <= len(open(tmp_dir + "/test.savnet.result.txt", 'r').readlines()) <= 104)
+        shutil.rmtree(tmp_dir)
 
 if __name__ == "__main__":
     unittest.main()
